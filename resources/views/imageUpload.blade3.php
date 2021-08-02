972e904f73d450c531576697b487b12379432b87
kad-er <kad-er>
2021-08-02 17:51:47 +0100

    finished

---
+++ resources/views/imageUpload.blade3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>laravel 8 image upload example - ItSolutionStuff.com.com</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+</head>
+
+<body>
+<div class="container">
+
+    
+       
+
+</div>
+</body>
+
+</html>
