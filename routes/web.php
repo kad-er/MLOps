6c2ba4a8c6c16178debd498119d4fa79b5a2d5a2
kad-er <kad-er>
2021-08-26 20:09:28 +0100

    .

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageUploadController;
+use App\Http\Controllers\ImageUploadControllerObjectDetection;
 
 /*
 |--------------------------------------------------------------------------
