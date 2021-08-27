5191842eea46415edc20c149de1d21c2894134e6
kad-er <kad-er>
2021-08-27 22:34:31 +0100

    .

--- resources/views/traitementImageMedicale.blade.php
+++ resources/views/traitementImageMedicale.blade.php
@@ -43,7 +43,7 @@
     <div class="container">
 
       <div class="logo float-left">
-        <h1 class="text-light"><a href="index.html"><span>Moderna</span></a></h1>
+        <h1 class="text-light"><a href="index.html"><span>MlOPS</span></a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
