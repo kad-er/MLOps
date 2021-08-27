c25ca1683f4c3036f204acf2595f3237af81c69f
kad-er <kad-er>
2021-08-27 21:18:14 +0100

    .

--- resources/views/traitementImageMedicale.blade.php
+++ resources/views/traitementImageMedicale.blade.php
@@ -95,13 +95,13 @@
               </div>
               <div class="cold-md-12">
                 <div class="col-md-6" >
-                  <img src="{{ Session::get('image') }}"/>
+                  <img src="{{ Session::get('image') }}?refreshed"/>
                 </div>
                 <div class="col-md-6 mx-auto my-auto" style="float: left;" >
-                  <a href="{{URL::to('/')}}/images/Output_img1.jpg" target="_blank">
+                  <a href="{{URL::to('/')}}/images/Output_img1.jpg?refreshed" target="_blank">
                       <button class="btn btn-primary"><i class="fa fa-download"></i> Download File jpg</button>
                   </a>
-                  <a href="{{URL::to('/')}}/images/Output_img1.nii.gz" target="_blank">
+                  <a href="{{URL::to('/')}}/images/Output_img1.nii.gz?refreshed" target="_blank">
                     <button class="btn btn-primary"><i class="fa fa-download"></i> Download File Nifti</button>
                 </a>
                 </div>
